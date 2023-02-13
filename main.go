abce62b38f45859a00d2cdd5bc84dda906a59d7a
Adrian Escat <adrian.escat@gmail.com>
2023-02-13 09:11:45 -0300

    Response headers and http methods
    See comments in the code

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	// Check if the current request URL path exactly matches "/". If it doesn't, use
-	// the http.NotFound() function to send a 404 response to the client.
-	// Importantly, we then return from the handler. If we don't return the handler
-	// would keep executing and also write the "Hello from letsgo" message.
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -23,6 +19,26 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", "POST") // Can be called just once per response
+		w.WriteHeader(405)              // Can be called just once per response
+
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body. Passing our http.ResponseWriter to
+		// another function, which sends a response to the user for us.
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
