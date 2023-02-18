374303113ef82db97f92204350f952bb52673828
Adrian Escat <adrian.escat@gmail.com>
2023-02-18 11:15:11 -0300

    Processing forms. Setting up a HTML form and parsing form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,82 +1,90 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"github.com/adrianescat/lets-go/internal/models"
-	"github.com/julienschmidt/httprouter" // New import
-	"net/http"
-	"strconv"
+  "errors"
+  "fmt"
+  "github.com/adrianescat/lets-go/internal/models"
+  "github.com/julienschmidt/httprouter" // New import
+  "net/http"
+  "strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// Because httprouter matches the "/" path exactly, we can now remove the
-	// manual check of r.URL.Path != "/" from this handler.
+  snippets, err := app.snippets.Latest()
+  if err != nil {
+	 app.serverError(w, err)
+	 return
+  }
 
-	snippets, err := app.snippets.Latest()
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+  data := app.newTemplateData(r)
+  data.Snippets = snippets
 
-	data := app.newTemplateData(r)
-	data.Snippets = snippets
-
-	app.render(w, http.StatusOK, "home.tmpl", data)
+  app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	// When httprouter is parsing a request, the values of any named parameters
-	// will be stored in the request context. We'll talk about request context
-	// in detail later in the book, but for now it's enough to know that you can
-	// use the ParamsFromContext() function to retrieve a slice containing these
-	// parameter names and values like so:
-	params := httprouter.ParamsFromContext(r.Context())
+  params := httprouter.ParamsFromContext(r.Context())
 
-	// We can then use the ByName() method to get the value of the "id" named
-	// parameter from the slice and validate it as normal.
-	id, err := strconv.Atoi(params.ByName("id"))
-	if err != nil || id < 1 {
+  id, err := strconv.Atoi(params.ByName("id"))
+  if err != nil || id < 1 {
+	 app.notFound(w)
+	 return
+  }
+
+  snippet, err := app.snippets.Get(id)
+  if err != nil {
+	 if errors.Is(err, models.ErrNoRecord) {
 		app.notFound(w)
-		return
-	}
+	 } else {
+		app.serverError(w, err)
+	 }
+	 return
+  }
 
-	snippet, err := app.snippets.Get(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			app.notFound(w)
-		} else {
-			app.serverError(w, err)
-		}
-		return
-	}
+  data := app.newTemplateData(r)
+  data.Snippet = snippet
 
-	data := app.newTemplateData(r)
-	data.Snippet = snippet
-
-	app.render(w, http.StatusOK, "view.tmpl", data)
+  app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
-// Add a new snippetCreate handler, which for now returns a placeholder
-// response. We'll update this shortly to show a HTML form.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+  data := app.newTemplateData(r)
+  app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Checking if the request method is a POST is now superfluous and can be
-	// removed, because this is done automatically by httprouter.
+  // First we call r.ParseForm() which adds any data in POST request bodies
+  // to the r.PostForm map. This also works in the same way for PUT and PATCH
+  // requests. If there are any errors, we use our app.ClientError() helper to
+  // send a 400 Bad Request response to the user.
+  err := r.ParseForm()
+  if err != nil {
+	 app.clientError(w, http.StatusBadRequest)
+	 return
+  }
 
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+  // Use the r.PostForm.Get() method to retrieve the title and content
+  // from the r.PostForm map.
+  title := r.PostForm.Get("title")
+  content := r.PostForm.Get("content")
 
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+  // The r.PostForm.Get() method always returns the form data as a *string*.
+  // However, we're expecting our expires value to be a number, and want to
+  // represent it in our Go code as an integer. So we need to manually covert
+  // the form data to an integer using strconv.Atoi(), and we send a 400 Bad
+  // Request response if the conversion fails.
+  expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+  if err != nil {
+	 app.clientError(w, http.StatusBadRequest)
+	 return
+  }
 
-	// Update the redirect path to use the new clean URL format.
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+  id, err := app.snippets.Insert(title, content, expires)
+
+  if err != nil {
+	 app.serverError(w, err)
+	 return
+  }
+
+  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,31 +7,21 @@ import (
 )
 
 func (app *application) routes() http.Handler {
-	// Initialize the router.
 	router := httprouter.New()
 
-	// Create a handler function which wraps our notFound() helper, and then
-	// assign it as the custom handler for 404 Not Found responses. You can also
-	// set a custom handler for 405 Method Not Allowed responses by setting
-	// router.MethodNotAllowed in the same way too.
 	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 	})
 
-	// Update the pattern for the route for the static files.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	// And then create the routes using the appropriate methods, patterns and
-	// handlers.
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreatePost)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
-	// Create the middleware chain as normal.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	// Wrap the router with the middleware and return it as normal.
 	return standard.Then(router)
 }
