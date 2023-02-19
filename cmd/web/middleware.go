78af28a0401178f04c5ec5c508e1317c5c532fc7
Adrian Escat <adrian.escat@gmail.com>
2023-02-19 00:22:15 -0300

    User Auth. CSRF protection

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "github.com/justinas/nosurf"
   "net/http"
 )
 
@@ -26,17 +27,10 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	 // Create a deferred function (which will always be run in the event
-	 // of a panic as Go unwinds the stack).
 	 defer func() {
-		// Use the builtin recover function to check if there has been a
-		// panic or not. If there has...
 		if err := recover(); err != nil {
-		  // Set a "Connection: close" header on the response.
 		  w.Header().Set("Connection", "close")
 
-		  // Call the app.serverError helper method to return a 500
-		  // Internal Server response.
 		  app.serverError(w, fmt.Errorf("%s", err))
 		}
 	 }()
@@ -47,20 +41,26 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 func (app *application) requireAuthentication(next http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	 // If the user is not authenticated, redirect them to the login page and
-	 // return from the middleware chain so that no subsequent handlers in
-	 // the chain are executed.
 	 if !app.isAuthenticated(r) {
 		http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 		return
 	 }
 
-	 // Otherwise set the "Cache-Control: no-store" header so that pages that
-	 // require authentication are not stored in the users browser cache (or
-	 // other intermediary cache).
 	 w.Header().Add("Cache-Control", "no-store")
 
-	 // And call the next handler in the chain.
 	 next.ServeHTTP(w, r)
   })
+}
+
+// Create a NoSurf middleware function which uses a customized CSRF cookie with
+// the Secure, Path and HttpOnly attributes set.
+func noSurf(next http.Handler) http.Handler {
+  csrfHandler := nosurf.New(next)
+  csrfHandler.SetBaseCookie(http.Cookie{
+	 HttpOnly: true,
+	 Path:     "/",
+	 Secure:   true,
+  })
+
+  return csrfHandler
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,40 +1,38 @@
 package main
 
 import (
-  "github.com/julienschmidt/httprouter"
-  "github.com/justinas/alice"
-  "net/http"
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+	"net/http"
 )
 
 func (app *application) routes() http.Handler {
-  router := httprouter.New()
+	router := httprouter.New()
 
-  router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	 app.notFound(w)
-  })
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
-  router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-  // Unprotected application routes using the "dynamic" middleware chain.
-  dynamic := alice.New(app.sessionManager.LoadAndSave)
-  
-  router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
-  router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-  router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
-  router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
-  router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
-  router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	// Use the nosurf middleware on all our 'dynamic' routes.
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
 
-  // Protected (authenticated-only) application routes, using a new "protected"
-  // middleware chain which includes the requireAuthentication middleware.
-  protected := dynamic.Append(app.requireAuthentication)
+	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 
-  router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
-  router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
-  router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
+	protected := dynamic.Append(app.requireAuthentication)
 
-  standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
-  return standard.Then(router)
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(router)
 }
